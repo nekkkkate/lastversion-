ad59af83622b39f8b1fd172343d1a837abc706a8
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-12-12 23:36:35 +0300

    file lastlaba added

---
+++ lastlaba.py
@@ -0,0 +1,9 @@
+from unittest import main
+
+from circletest import CircleTestCase
+from rectangletest import RectangleTestCase
+from squaretest import SquareTestCase
+from triangletests import TriangleTestCase
+
+if __name__ == "__main__":
+    main()
