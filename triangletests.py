73ff8dc67950a5e74a3cc7f5c9f1c545204e313b
nekkkkate <144551827+nekkkkate@users.noreply.github.com>
2023-12-12 23:04:22 +0300

    tests for triangle

---
+++ triangletests.py
@@ -0,0 +1,19 @@
+import unittest
+from triangle import area
+from triangle import perimeter
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(8, 0)
+        self.assertEqual(res, 0)
+
+    def test_triangle_mul(self):
+        res = area(8, 12)
+        self.assertEqual(res, 48)
+
+    def test_zero_per(self):
+        res = perimeter(8, 0, 12)
+        self.assertEqual(res, 20)
+
+    def test_triangle_per(self):
+        res = perimeter(6, 4, 3)
+        self.assertEqual(res, 13)
